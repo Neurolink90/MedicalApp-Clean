248cc553256948c778c3f78f6c734e51fa0938b5
Neurolink90 <94756842+Neurolink90@users.noreply.github.com>
2026-02-19 06:14:56 -0500

    Built web
    Branch: main
    Commit: ca13ae0267628c6199949d31ccb6e5b4b924dc01 (dirty)
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "818603611"
+    serviceWorkerVersion: "3334793339"
   }
 });
