1cf3387f30505148c5b35d10b30e6d519f579d9d
Neurolink90 <94756842+Neurolink90@users.noreply.github.com>
2026-03-15 21:41:45 -0400

    Built web
    Branch: main
    Commit: fbd4439118d65007ae2d386ed374779f31ce104b (dirty)
    
    package:peanut 6.0.0

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,21 +1,19 @@
 importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js");
 
-// Use the same config here
 const firebaseConfig = {
-    apiKey: "AIzaSyDu-DaxMY4O_IaRyPKM7DVigyi7zo1IntA",
-    authDomain: "medirecords-pro.firebaseapp.com",
-    projectId: "medirecords-pro",
-    storageBucket: "medirecords-pro.firebaseasestorage.app",
-    messagingSenderId: "379331373787",
-    appId: "1:379331373787:web:33555fcacc7fa3a92a7afe",
-    measurementId: "G-61P8TKS1V2"
+  apiKey: "AIzaSyDu-DaxmY4O_IaRyPKM7DVigyi7zo1INtA", // UPDATED: Uppercase 'N'
+  authDomain: "medirecords-pro.firebaseapp.com",
+  projectId: "medirecords-pro",
+  storageBucket: "medirecords-pro.firebasestorage.app", // FIXED: Removed 'ase'
+  messagingSenderId: "379331373787",
+  appId: "1:379331373787:web:33555fcacc7fa3a92a7afe",
+  measurementId: "G-61P8TKS1V2"
 };
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// Handle background notifications
 messaging.onBackgroundMessage((payload) => {
   console.log('Received background message ', payload);
   const notificationTitle = payload.notification.title;
