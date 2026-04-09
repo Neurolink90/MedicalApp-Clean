cc5b61e038e56cde99eea56156b14d8fe7b080f3
Neurolink90 <94756842+Neurolink90@users.noreply.github.com>
2026-04-09 06:48:11 -0400

    Built web
    Branch: main
    Commit: ce219f98f9b023ac80fc5f6179b660b076882a48 (dirty)
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0f4dc712b37274bea28ec5d5af2f018f
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "firebase-messaging-sw.js": "bef51a446359c6619dd6b6c8a37e52ba",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "1171dee80fa1b0ffe95a70c8a41bebd1",
+"flutter_bootstrap.js": "36afee5be688b375c41618bfa76386ef",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
